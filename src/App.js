b8f2afebaa81486cd87480c9ce101c6d8749aaff
Timtim <timtim@Pintus-MacBook-Air.local>
2023-04-19 22:45:01 +0530

    image practice

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Image from './Image';
 
 function App() {
   return (
@@ -18,6 +19,8 @@ function App() {
           Learn React
         </a>
       </header>
+      <Image></Image>
+      
     </div>
   );
 }
---
+++ src/Image.js
@@ -0,0 +1,7 @@
+function Image () {
+    return <>
+      <img src="https://www.shutterstock.com/image-illustration/hindu-lord-radha-kishana-texture-600w-1468036604.jpg"></img>
+     <img src="https://www.shutterstock.com/image-illustration/lord-radha-krishna-hindu-god-600w-1440735725.jpg"></img>
+     </>
+}
+export default Image
