9b11cbd7f1022dba0244dce5a82787cbc68d4926
Timtim <timtim@Pintus-MacBook-Air.local>
2023-05-30 15:45:02 +0530

    form module

--- src/form.js
+++ src/form.js
@@ -10,29 +10,60 @@ function Form() {
         <Input placeholder={"Name*"} />
         <Input placeholder={"Mobile*"} />
       </div>
-      <div className={styles.chrome}>
+      <div className={styles.fackbook}>
         <Input placeholder={"Stara*"} />
         <Input placeholder={"Address(House No,Building street,Area)*"} />
         <Input placeholder={"Locality/town*"} />
         <Input placeholder={"City/District*"} />
       </div>
-      <div className={styles.look}>
+      <div className={styles.fackbook}>
         <p>Type of Address *</p>
-        <label for="male">
-          <input type="radio"></input> Home{" "}
+        <label className={styles.house}>
+          <input
+            name="address-type"
+            type="radio"
+            className={styles.house}
+          ></input>
+          Home
         </label>
-        <label for="male">
-          <input type="radio"></input> Office{" "}
+        <label className={styles.bool}>
+          <input
+            name="address-type"
+            type="radio"
+            className={styles.house}
+          ></input>
+          Office
         </label>
         <p>Is your office open on weekends?</p>
-        <input type="checkbox" />
-        <label> Open on Saturday</label>
-        <br></br>
-        <input type="checkbox" />
-        <label> Open on Sunday</label>
-        <br></br>
-        <input type="checkbox" />
-        <label> Open on Sunday</label>
+        <div>
+          <div>
+            <label className={styles.himanshu}>
+              <input type="checkbox" className={styles.not} />
+              Open on Saturday
+            </label>
+          </div>
+          <div>
+            <label className={styles.himanshu}>
+              <input type="checkbox" className={styles.not} />
+              Open on Sunday
+            </label>
+          </div>
+          <hr className={styles.abhi}></hr>
+          <div className={styles.top}>
+            <label className={styles.himanshu}>
+              <input type="checkbox" className={styles.not} />
+              Make this as my default address
+            </label>
+          </div>
+        </div>
+      </div>
+      <div className={styles.fackbook + " " + styles["footer-button"]}>
+        <button className={styles.button}>
+          <b>CANCEL</b>
+        </button>{" "}
+        <button className={styles.tag}>
+          <b className={styles.two}>SAVE</b>
+        </button>
       </div>
     </div>
   );
